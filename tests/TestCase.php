cf4188388fe4c2961a349bee3476ac944c326112
adamhut <adamhut@gmail.com>
2021-12-29 14:57:25 -0800

    use phpunit

---
+++ tests/TestCase.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Adamhut\WeightConversions\Tests;
+
+use PHPUnit\Framework\TestCase as BaseCase;
+
+abstract class TestCase extends BaseCase
+{
+}
--- tests/WeightTest.php
+++ tests/WeightTest.php
@@ -1,7 +1,26 @@
 <?php
 
-it('it can covert kilogram to pounds', function () {
-    $pounds = \Adamhut\WeightConversions\Weight::kilograms(100)->toPounds();
+namespace Adamhut\WeightConversions\Tests;
 
-    expect($pounds)->toEqual(220.46);
-});
+class WeightTest extends TestCase
+{
+    /** @test */
+    public function it_can_test()
+    {
+        $this->assertTrue(true);
+    }
+
+    /** @test */
+    public function it_can_covert_kilogram_to_pounds()
+    {
+        $pounds = \Adamhut\WeightConversions\Weight::kilograms(100)->toPounds();
+        $this->assertEquals(220.46, $pounds);
+    }
+}
+
+
+// it('it can covert kilogram to pounds', function () {
+//     $pounds = \Adamhut\WeightConversions\Weight::kilograms(100)->toPounds();
+
+//     expect($pounds)->toEqual(220.46);
+// });
