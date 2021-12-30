b8307d2e46b0a9807fbb32b626947e83f3f73337
adamhut <adamhut@gmail.com>
2021-12-29 16:38:49 -0800

    update test description

--- tests/WeightTest.php
+++ tests/WeightTest.php
@@ -1,6 +1,6 @@
 <?php
 
-it('it can covert kilogram to pounds', function () {
+it('it can covert kilogram to pounds correctly', function () {
     $pounds = \Adamhut\WeightConversions\Weight::kilograms(100)->toPounds();
 
     expect($pounds)->toEqual(220.46);
