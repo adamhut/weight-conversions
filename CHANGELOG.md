e427b587e7e1c0fce0b3b266fda82f2adeba6964
adamhut <adamhut@gmail.com>
2021-12-29 20:02:08 -0800

    add kilo to stones

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 All notable changes to `weight-conversions` will be documented in this file.
 
+## 1.2.0 - 2021-12-29
+
+- add kilo to stones
+
 ## 1.1.0 - 2021-12-29
 
 - add pound to kg
--- tests/WeightTest.php
+++ tests/WeightTest.php
@@ -6,6 +6,12 @@ it('it can covert kilogram to pounds correctly', function () {
     expect($pounds)->toEqual(220.46);
 });
 
+it('it can covert kilogram to stones correctly', function () {
+    $stones = \Adamhut\WeightConversions\Weight::kilograms(100)->toStones();
+
+    expect($stones)->toEqual(15.747);
+});
+
 
 it('it can covert pounds to kilo correctly', function () {
     $kilo = \Adamhut\WeightConversions\Weight::poundToKilograms(100);
